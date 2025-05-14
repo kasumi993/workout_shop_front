23b2c0ba8cfb3fc023c16489bfa37bd66f3563e2
Khady GUEYE <42519251+kasumi993@users.noreply.github.com>
2025-05-14 02:08:27 +0200

    update addtocart btn

--- src/components/buttons/addToCartBtn.js
+++ src/components/buttons/addToCartBtn.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import { useRef } from 'react';
 import { CartContext } from '@/context/CartContext';
 import { useContext} from 'react';
 import { HiOutlineShoppingCart } from 'react-icons/hi';
 
 const AddToCartBtn = ({ className,iconClass, productId, onAddToCart }) => {
     const { addProduct } = useContext(CartContext);
+    const buttonRef = useRef(null);
 
     // Function to handle adding the product to the cart
     const handleAddToCart = () => {
         addProduct(productId);
-        onAddToCart(productId);
+        // Change button text temporarily to show feedback
+        if (buttonRef.current) {
+            const originalText = buttonRef.current.textContent;
+            buttonRef.current.textContent = "Ajouté !";
+            setTimeout(() => {
+                buttonRef.current.textContent = originalText;
+            }, 3000);
+        }
+
+        // Call the onAddToCart callback if provided
+        if (onAddToCart) {
+            onAddToCart(productId);
+        }
     };
 
-    const buttonClass = `flex gap-3 bg-black hover:bg-primary text-white font-light py-2 px-5 rounded-md ${className}`;
-
     return (
-        <button 
-            className={`${className} flex gap-3 bg-black hover:bg-primary text-white font-light py-2 px-5 rounded-md `}
+        <button ref={buttonRef}
+            className={`${className ? className : ''} flex gap-3 bg-gray-900 hover:scale-105 hover:shadow-xl text-white font-light py-2 px-5 rounded-md cursor-pointer transition duration-300 ease-in-out`}
             onClick={() => handleAddToCart(productId)}
         >
             <HiOutlineShoppingCart  className={`${iconClass} text-[24px]`}/>
